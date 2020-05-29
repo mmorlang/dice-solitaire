294ea77ddceff964c1a446abcf8e84ffba8d6521
Mickie Morlang <mickiemorlang@gmail.com>
2020-05-29 12:00:10 -0600

    added base lines

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -1,6 +1,9 @@
 package edu.cnm.deepdive.dicesolitaire;
 
 import android.content.res.Resources;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.annotation.NonNull;
@@ -9,6 +12,7 @@ import android.os.Bundle;
 import edu.cnm.deepdive.dicesolitaire.R;
 import edu.cnm.deepdive.dicesolitaire.model.Roll;
 import java.text.NumberFormat;
+import java.util.Arrays;
 import java.util.Random;
 
 public class MainActivity extends AppCompatActivity {
@@ -20,6 +24,9 @@ public class MainActivity extends AppCompatActivity {
   private int maxPairValue;
   private TextView[] labels;
   private ProgressBar[] counts;
+  private Button roller;
+  private TextView rollDisplay;
+  private Random rng;
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -29,7 +36,7 @@ public class MainActivity extends AppCompatActivity {
     labels = new TextView[maxPairValue - minPairValue + 1];
     counts = new ProgressBar[maxPairValue - minPairValue + 1];
     Resources res = getResources();
-    Random rng = new Random();
+    rng = new Random();
     NumberFormat formatter = NumberFormat.getInstance();
     for (int i = minPairValue; i <= maxPairValue; i++) {
       String labelIdString = String.format(LABEL_ID_FORMAT, i);
@@ -40,6 +47,18 @@ public class MainActivity extends AppCompatActivity {
       int countId = res.getIdentifier(countIdString, "id", getPackageName());
       counts[i - minPairValue] = findViewById(countId);
       counts[i - minPairValue].setProgress(1 + rng.nextInt(10));
+    }
+    roller = findViewById(R.id.roller);
+    rollDisplay = findViewById(R.id.roll_display);
+    roller.setOnClickListener(new RollerListener());
+  }
+
+  private class RollerListener implements OnClickListener{
+
+    @Override
+    public void onClick(View v) {
+     Roll roll = new Roll(rng);
+     rollDisplay.setText(Arrays.toString(roll.getDice()));
     }
   }
 
