8e1798ab7286240af62a4c3e30578a27f3f7171e
Mickie Morlang <mmorlang@gmail.com>
2020-06-03 14:25:53 -0600

    Wired fields to dice ImageView objects.

--- app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/dicesolitaire/MainActivity.java
@@ -1,13 +1,17 @@
 package edu.cnm.deepdive.dicesolitaire;
 
+import static edu.cnm.deepdive.dicesolitaire.model.Roll.NUM_FACES;
+
 import android.content.res.Resources;
+import android.graphics.drawable.Drawable;
+import android.os.Bundle;
 import android.view.View;
 import android.view.View.OnClickListener;
 import android.widget.Button;
+import android.widget.ImageView;
 import android.widget.ProgressBar;
 import android.widget.TextView;
 import androidx.appcompat.app.AppCompatActivity;
-import android.os.Bundle;
 import edu.cnm.deepdive.dicesolitaire.model.Roll;
 import java.text.NumberFormat;
 import java.util.Arrays;
@@ -19,17 +23,19 @@ public class MainActivity extends AppCompatActivity {
   private static final String PAIR_COUNT_ID_FORMAT = "pair_%d_count";
   private static final String SCRATCH_LABEL_ID_FORMAT = "scratch_%d_label";
   private static final String SCRATCH_COUNT_ID_FORMAT = "scratch_%d_count";
-
+  private static final String DIE_IMAGE_ID_FORMAT = "die_%d";
+  private static final String DICE_FACE_ID_FORMAT = "face_%d";
 
   private int minPairValue = 2;
-  private int maxPairValue = 2 * Roll.NUM_FACES;
+  private int maxPairValue = 2 * NUM_FACES;
   private TextView[] pairLabels;
   private ProgressBar[] pairCounts;
-  private Button roller;
-  private Random rng = new Random();
   private TextView[] scratchLabels;
   private ProgressBar[] scratchCounts;
-
+  private ImageView[] diceImages;
+  private Drawable[] diceFaces;
+  private Button roller;
+  private Random rng = new Random();
 
   @Override
   protected void onCreate(Bundle savedInstanceState) {
@@ -42,16 +48,16 @@ public class MainActivity extends AppCompatActivity {
     Resources res = getResources();
     NumberFormat formatter = NumberFormat.getInstance();
     setupPairControls(res, formatter);
-    setupPlayControls();
+    setupPlayControls(res);
     setupScratchControls(res, formatter);
   }
 
   private void setupScratchControls(Resources res, NumberFormat formatter) {
-    scratchLabels = new TextView[Roll.NUM_FACES];
-    scratchCounts = new ProgressBar[Roll.NUM_FACES];
-    for (int i = 1; i <= Roll.NUM_FACES; i++) {
-      String scratch_1_label = String.format(SCRATCH_LABEL_ID_FORMAT, i);
-      int labelId = res.getIdentifier(scratch_1_label, "id", getPackageName());
+    scratchLabels = new TextView[NUM_FACES];
+    scratchCounts = new ProgressBar[NUM_FACES];
+    for (int i = 1; i <= NUM_FACES; i++) {
+      String labelIdString = String.format(SCRATCH_LABEL_ID_FORMAT, i);
+      int labelId = res.getIdentifier(labelIdString, "id", getPackageName());
       scratchLabels[i - 1] = findViewById(labelId);
       scratchLabels[i - 1].setText(formatter.format(i));
       String countIdString = String.format(SCRATCH_COUNT_ID_FORMAT, i);
@@ -61,15 +67,27 @@ public class MainActivity extends AppCompatActivity {
     }
   }
 
-  private void setupPlayControls() {
+  private void setupPlayControls(Resources res) {
     roller = findViewById(R.id.roller);
-    // TODO find and wire up dice image vie
-    //  w objects
+    diceImages = new ImageView[Roll.NUM_DICE];
+    for (int i = 0; i < Roll.NUM_DICE; i++) {
+      String idString = String.format(DIE_IMAGE_ID_FORMAT, i + 1);
+      int id = res.getIdentifier(idString, "id", getPackageName());
+      diceImages[i] = findViewById(id);
+      diceImages[i].setImageDrawable(getDrawable(R.drawable.face_6));
+    }
+    diceFaces = new Drawable[Roll.NUM_FACES];
+    for (int i = 0; i < Roll.NUM_FACES; i++) {
+      String idString = String.format(DICE_FACE_ID_FORMAT, i + 1);
+      int id = res.getIdentifier(idString, "drawable", getPackageName());
+      Drawable face = getDrawable(id);
+      diceFaces[i] = face;
+    }
     roller.setOnClickListener(new OnClickListener() {
       @Override
       public void onClick(View v) {
         Roll roll = new Roll(rng);
-        // TODO display dice images.
+        // TODO Display dice images.
       }
     });
   }
@@ -90,4 +108,4 @@ public class MainActivity extends AppCompatActivity {
   }
 
 
-  }
+}
